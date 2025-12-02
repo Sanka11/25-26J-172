0aaca34cbc26da020eb6c8506000ac5113a1699c
Savindi500 <ssavindi660@gmail.com>
2025-12-02 22:12:29 +0530

    feat(backend): added recommendation feature

--- backend/functions/src/index.js
+++ backend/functions/src/index.js
@@ -1,6 +1,9 @@
 // backend/functions/src/index.js
 
-// Add other requires/exports here if you have more functions in future
+
+  // Add other requires/exports here if you have more functions in future
 const { predictRisk } = require("./http/mlProxy");
+const { predictRecommendation } = require("./http/mlRecommendationProxy");
 
 exports.predictRisk = predictRisk;
+exports.predictRecommendation = predictRecommendation;
