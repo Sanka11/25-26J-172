04d1c0856a2e06f00693792a8ffd8ddd2809c30f
Savindi500 <ssavindi660@gmail.com>
2025-12-29 16:33:58 +0530

    feat(gamification): add quiz level-up system and UI improvements

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,3 @@
-// import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-// import RiskDemo from "./pages/RiskDemo";
-// import RecommendationDemo from "./pages/RecommendationDemo";
-
-// function App() {
-//   return (
-//     <BrowserRouter>
-//       <nav className="p-4 bg-gray-200 flex gap-4">
-//         <Link to="/risk">Risk Demo</Link>
-//         <Link to="/recommendation">Recommendation Demo</Link>
-//       </nav>
-
-//       <Routes>
-//         <Route path="/risk" element={<RiskDemo />} />
-//         <Route path="/recommendation" element={<RecommendationDemo />} />
-
-//         {/* Default: go to risk page */}
-//         <Route path="/" element={<RiskDemo />} />
-//       </Routes>
-//     </BrowserRouter>
-//   );
-// }
-
-// export default App;
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import RiskDemo from "./pages/RiskDemo";
 import RecommendationDemo from "./pages/RecommendationDemo";
@@ -29,8 +5,7 @@ import RecommendationDemo from "./pages/RecommendationDemo";
 import CreateQuiz from "./pages/CreateQuiz";
 import QuizList from "./pages/QuizList";
 import TakeQuiz from "./pages/TakeQuiz";
-import StruggleDashboard from "./pages/StruggleDashboard";
-import QuizLevels from "./pages/QuizLevels";
+import Levels from "./pages/Levels";
 
 function App() {
   return (
@@ -40,20 +15,22 @@ function App() {
         <Link to="/recommendation">Recommendation Demo</Link>
         <Link to="/create-quiz">Create Quiz</Link>
         <Link to="/quizzes">Quizzes</Link>
-        <Link to="/struggles">My Struggles</Link>
+        
       </nav>
 
       <Routes>
         <Route path="/risk" element={<RiskDemo />} />
         <Route path="/recommendation" element={<RecommendationDemo />} />
 
+        <Route path="/" element={<Levels currentLevel={1} />} />
+        <Route path="/levels" element={<Levels currentLevel={1} />} />
         <Route path="/create-quiz" element={<CreateQuiz />} />
         <Route path="/quizzes" element={<QuizList />} />
-        <Route path="/quiz/:id" element={<TakeQuiz />} />
-        <Route path="/struggles" element={<StruggleDashboard />} />
-        <Route path="/levels" element={<QuizLevels />} />
+        <Route path="/quiz/:level" element={<TakeQuiz />} />
+        
+        
 
-        <Route path="/" element={<RiskDemo />} />
+        
       </Routes>
     </BrowserRouter>
   );
